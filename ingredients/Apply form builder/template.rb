6192990ac572200bbce65584672595d5af633056
railsnew.io <trinity@railsnew.io>
2025-04-06 11:53:18 +0200

    Update ingredient: Apply form builder

--- ingredients/Apply form builder/template.rb
+++ ingredients/Apply form builder/template.rb
@@ -1,6 +1,6 @@
 prepend_to_file 'app/views/sessions/new.html.erb', <<~SNIPPET_1
   <div class="flex flex-col items-center justify-center h-full">
-  <h1 class="bg-white py-px px-2 -mx-2 uppercase font-bold text-2xl">Sign in</h1>
+  <h1 class="bg-white mt-10 py-px px-2 -mx-2 uppercase font-bold text-2xl">Sign in</h1>
 SNIPPET_1
 
 
