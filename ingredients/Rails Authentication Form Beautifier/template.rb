b9a3446114b03db6a132576a8e015f6ff8dfba30
railsnew.io <trinity@railsnew.io>
2025-04-07 09:57:16 +0200

    Update ingredient: Rails Authentication Form Beautifier

--- ingredients/Rails Authentication Form Beautifier/template.rb
+++ ingredients/Rails Authentication Form Beautifier/template.rb
@@ -3,8 +3,8 @@ empty_directory "app/form_builders"
 create_file "app/form_builders/tailwind_authentication.rb", <<~SNIPPET_1
 class TailwindAuthentication < ActionView::Helpers::FormBuilder
   FORM_STYLE = "mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-4"
-  TEXT_FIELD_STYLE = "w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-  SUBMIT_STYLE = "flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+  TEXT_FIELD_STYLE = "w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-rose-700 sm:text-sm/6"
+  SUBMIT_STYLE = "flex w-full justify-center rounded-md bg-rose-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-rose-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
 
   def initialize(object_name, object, template, options)
     options[:html] = {}
