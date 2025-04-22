69ab941387290e0631b4961c9a9d3d75330ce0f9
railsnew.io <trinity@railsnew.io>
2025-04-22 09:45:36 +0200

    Update ingredient: RSpec

--- ingredients/RSpec/template.rb
+++ ingredients/RSpec/template.rb
@@ -2,7 +2,17 @@ gem_group :development, :test do
   gem "rspec-rails"
 end
 
+puts "DEBUG: About to run bundle install"
+puts "DEBUG: Current directory: #{Dir.pwd}"
+puts "DEBUG: Gemfile contents:"
+puts File.read("Gemfile")
+puts "DEBUG: Bundler version: #{Bundler::VERSION}"
+puts "DEBUG: Ruby version: #{RUBY_VERSION}"
+
 run "bundle install"
+
+puts "DEBUG: Bundle install completed"
+
 generate "rspec:install"
 
 run "rm -rf test"
